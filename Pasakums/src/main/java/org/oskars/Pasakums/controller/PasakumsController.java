db1c3d0cdf526cef7467a1dad81b521964e19a5e
Elles96 <oskarsdudurs@gmail.com>
2025-12-06 22:17:49 +0200

    backend1

--- Pasakums/src/main/java/org/oskars/Pasakums/controller/PasakumsController.java
+++ Pasakums/src/main/java/org/oskars/Pasakums/controller/PasakumsController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/pasakumi")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PasakumsController {
 
     private final PasakumsService service;
